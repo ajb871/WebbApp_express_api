dd4ab9e3b23cb96dd2284232d3d8ad2140866c3a
ajb871 <ajb871@nyu.edu>
2018-11-04 19:06:38 -0500

    request data from API

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const app = express();
+const body_parser = require('body-parser');
 const ejs = require('ejs');
 const request = require('request');
+
+app.use(body_parser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 
 app.get('/',(req,res)=>{
-	res.render('index');
+	res.render('index', {sun_data: null});
+});
+
+app.post('/',(req,res)=>{
+	console.log('request: ' + req.body.latitude + ', '+ req.body.longitude);
+	let lat = req.body.latitude;
+	let lng = req.body.longitude
+	let url = `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}`;
+	request(url,(err,resp,body)=>{
+		if(err){
+			console.log(`Error: ${err}`);
+			res.render('index', {sun_data: null});
+		} else {
+			let data = JSON.parse(body);
+			if(data.results == undefined){
+				console.log(`Error: ${err}`);
+				res.render('index', {sun_data: null});
+			} else {
+				console.log('request success: ');
+				console.log(data.results);
+				res.render('index', {sun_data: data})
+			}
+		}
+	});
 });
 
 app.listen(3000,()=>{
